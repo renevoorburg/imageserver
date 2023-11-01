40efc949ebf893a9ab47db8726464cad999c4256
renevoorburg <voorburg@xs4all.nl>
2023-11-01 23:00:48 +0100

    extending Imagick

--- src/ImageProcessor.php
+++ src/ImageProcessor.php
@@ -1,67 +1,50 @@
 <?php
 
-class ImageProcessor
+class ImageProcessor extends Imagick
 {
-    private Imagick $image;
 
     public function __construct(string $source_image)
     {
-        $this->image = new Imagick($source_image);
-    }
-
-    public function autoRotateImage()
-    {
-        switch ($this->image->getImageOrientation()) {
-            case imagick::ORIENTATION_BOTTOMRIGHT:
-                $this->image->rotateimage("#000", 180); // rotate 180 degrees
-                break;
-            case imagick::ORIENTATION_RIGHTTOP:
-                $this->image->rotateimage("#000", 90); // rotate 90 degrees CW
-                break;
-            case imagick::ORIENTATION_LEFTBOTTOM:
-                $this->image->rotateimage("#000", -90); // rotate 90 degrees CCW
-                break;
-        }
-        // ensure the EXIF data is correct:
-        $this->image->setImageOrientation(imagick::ORIENTATION_TOPLEFT);
+        parent::__construct($source_image);
+        $this->autoRotateImage();
     }
 
     public function contain($requested_width, $requested_height)
     {
         // 0 -preserve aspect ratio, fit image in contains of specified box:
         $new_width = $requested_width ;
-        $new_height = $this->image->getImageHeight() * $new_width / $this->image->getImageWidth();
+        $new_height = $this->getImageHeight() * $new_width / $this->getImageWidth();
         if ($new_height > $requested_height) {
             $new_height = $requested_height;
-            $new_width = $this->image->getImageWidth() * $new_height / $this->image->getImageHeight();
+            $new_width = $this->getImageWidth() * $new_height / $this->getImageHeight();
         }
-        $this->image->resizeImage((int)$new_width, (int)$new_height, imagick::FILTER_LANCZOS, 1);
+        $this->resizeImage((int)$new_width, (int)$new_height, imagick::FILTER_LANCZOS, 1);
     }
 
     public function cover($requested_width, $requested_height)
     {
         // 6 -preserve aspect ratio, fit image to fully cover specified box:
-        $factor_height = $requested_height / $this->image->getImageHeight();
-        $factor_width = $requested_width / $this->image->getImageWidth();
+        $factor_height = $requested_height / $this->getImageHeight();
+        $factor_width = $requested_width / $this->getImageWidth();
         $factor = ($factor_height < $factor_width) ? $factor_width : $factor_height;
-        $new_width = (int) ($this->image->getImageWidth() * $factor);
-        $new_height = (int) ($this->image->getImageHeight() * $factor);
+        $new_width = (int) ($this->getImageWidth() * $factor);
+        $new_height = (int) ($this->getImageHeight() * $factor);
 
-        $this->image->resizeImage((int)$new_width, (int)$new_height, imagick::FILTER_LANCZOS, 1);
+        $this->resizeImage((int)$new_width, (int)$new_height, imagick::FILTER_LANCZOS, 1);
     }
 
     public function crop($requested_width, $requested_height)
     {
         $requested_aspect_rate = $requested_width / $requested_height;
-        $source_aspect_rate = $this->image->getImageWidth() / $this->image->getImageHeight();
+        $source_aspect_rate = $this->getImageWidth() / $this->getImageHeight();
         if ($requested_aspect_rate > $source_aspect_rate) {
-            $trim = $this->image->getImageHeight() - ($this->image->getImageWidth() / $requested_aspect_rate);
-            $this->image->cropImage((int)$this->image->getImageWidth(), (int)($this->image->getImageHeight() - $trim), 0, (int)($trim/2));
+            $trim = $this->getImageHeight() - ($this->getImageWidth() / $requested_aspect_rate);
+            $this->cropImage((int)$this->getImageWidth(), (int)($this->getImageHeight() - $trim), 0, (int)($trim/2));
         } else {
-            $trim = $this->image->getImageWidth() - ($this->image->getImageHeight() * $requested_aspect_rate);
-            $this->image->cropImage((int)($this->image->getImageWidth() - $trim), (int)$this->image->getImageHeight(), (int)($trim/2), 0);
+            $trim = $this->getImageWidth() - ($this->getImageHeight() * $requested_aspect_rate);
+            $this->cropImage((int)($this->getImageWidth() - $trim), (int)$this->getImageHeight(), (int)($trim/2), 0);
         }
-        $this->image->resizeImage((int)$requested_width, (int)$requested_height, imagick::FILTER_LANCZOS, 1);
+        $this->resizeImage((int)$requested_width, (int)$requested_height, imagick::FILTER_LANCZOS, 1);
     }
 
     public function size($requested_width, $requested_height)
@@ -69,22 +52,22 @@ class ImageProcessor
         if (isset($requested_width) && !isset($requested_height)) {
             //  3 -preserve aspect ratio, fit width to specified box:
             $new_width = $requested_width;
-            $new_height = $this->image->getImageHeight() * $requested_width / $this->image->getImageWidth();
+            $new_height = $this->getImageHeight() * $requested_width / $this->getImageWidth();
         } elseif (isset($requested_height) && !isset($requested_width)) {
             // 4 -preserve aspect ratio, fit height to specified box:
             $new_height = $requested_height;
-            $new_width = $this->image->getImageWidth() * $requested_height / $this->image->getImageHeight();
+            $new_width = $this->getImageWidth() * $requested_height / $this->getImageHeight();
         } else {
             // 5 - fit to specified box, regardless the aspect ratio:
             $new_width = $requested_width;
             $new_height = $requested_height;
         }
-        $this->image->resizeImage((int)$new_width, (int)$new_height, imagick::FILTER_LANCZOS, 1);
+        $this->resizeImage((int)$new_width, (int)$new_height, imagick::FILTER_LANCZOS, 1);
     }
 
     public function getHeader() : string
     {
-        switch ($this->image->getImageFormat()) {
+        switch ($this->getImageFormat()) {
             case 'JPEG':
                 return('Content-Type: image/jpeg');
             case 'PNG':
@@ -95,8 +78,21 @@ class ImageProcessor
         return '';
     }
 
-    public function getImageBlob()
+    private function autoRotateImage()
     {
-        return $this->image->getImageBlob();
+        switch ($this->getImageOrientation()) {
+            case imagick::ORIENTATION_BOTTOMRIGHT:
+                $this->rotateimage("#000", 180); // rotate 180 degrees
+                break;
+            case imagick::ORIENTATION_RIGHTTOP:
+                $this->rotateimage("#000", 90); // rotate 90 degrees CW
+                break;
+            case imagick::ORIENTATION_LEFTBOTTOM:
+                $this->rotateimage("#000", -90); // rotate 90 degrees CCW
+                break;
+        }
+        // ensure the EXIF data is correct:
+        $this->setImageOrientation(imagick::ORIENTATION_TOPLEFT);
     }
+
 }
